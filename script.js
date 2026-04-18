7f8ae55aea12f251d41c849f0dc57a7300acd15f
Tamara520 <2024520@student.cct.ie>
2026-04-18 20:39:18 +0100

    Enhanced JavaScript functionality

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-console.log("Food Delivery App loaded");
+// App loaded message
+console.log("Food Delivery App loaded successfully");
 
-document.querySelector("button").addEventListener("click", function() {
+// Welcome message when page loads
+window.onload = function () {
+    alert("Welcome to Food Delivery App!");
+};
+
+// Button click event
+document.querySelector("button").addEventListener("click", function () {
     alert("Order feature coming soon!");
 });
+
+// Simple feature log
+console.log("User interface initialized");
+
+// Future feature placeholder
+function orderFood() {
+    console.log("Order function will be implemented later");
+}
